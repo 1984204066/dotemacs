f87d4721a54e3c61f51107226a6e8692b381f207
1984204066 <1984204066@qq.com>
2022-01-19 22:25:33 +0800

    remove init-projctile.el

--- lisp/init-prog-mode.el
+++ lisp/init-prog-mode.el
@@ -2,11 +2,143 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package init-projectile
-  :defer nil
-  :load-path "./lisp"
-  ;; :hook (after-init . 'projectile-mode)
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   'simple
+          treemacs-file-event-delay                5000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-find-workspace-method           'find-for-file-or-pick-first
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           35
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
+
+    (treemacs-hide-gitignored-files-mode nil))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+;; (use-package treemacs-evil
+;;   :after (treemacs evil)
+;;   :ensure t)
+
+;; (use-package init-projectile
+;;   :defer nil
+;;   :load-path "./lisp"
+;;   ;; :hook (after-init . 'projectile-mode)
+;;   )
+
+(use-package projectile
+  ;; https://github.com/jwiegley/use-package#extending-the-load-path
+  :bind-keymap ("C-c p" . projectile-command-map)  ;;default is?.
+  :init (setq projectile-use-git-grep t)
+  :config
+  ;; Shorter modeline
+  (setq-default projectile-mode-line-prefix " Proj")
+  (when (executable-find "rg")
+    (setq-default projectile-generic-command "rg --files --hidden"))
+  (setq prj-cache-file
+	(expand-file-name ".cache/projectile/projects.cache" user-emacs-directory))
+  (message "projects cache file = %s" prj-cache-file)
+  (setq
+   ;;   projectile-enable-caching t
+   ;;   projectile-indexing-method 'alien
+   ;;   projectile-require-project-root t
+   ;;   projectile-globally-ignored-file-suffixes '(".elc" ".pyc" ".o" ".Po")
+   ;;   ;;projectile-globally-ignored-directories '(".deps")
+   projectile-completion-system 'helm ;;经常用ivy https://www.jianshu.com/p/250301ebb7f9
+   )
+  (append '(".deps" "*.cache" "*.git" "Downloads" "Documents") projectile-globally-ignored-directories)
+  (use-package helm-projectile)
+  (use-package ibuffer-projectile))
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :init
+  (projectile-mode t)
   )
+
+;; (use-package treemacs-icons-dired
+;;   :hook (dired-mode . treemacs-icons-dired-enable-once)
+;;   :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+;; (use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+;;   :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+;;   :ensure t
+;;   :config (treemacs-set-scope-type 'Perspectives))
 
 ;; https://robert.kra.hn/posts/2021-02-07_rust-with-emacs/
 (use-package rustic
@@ -50,6 +182,7 @@
   (lsp-idle-delay 0.6)
   (lsp-rust-analyzer-server-display-inlay-hints t)
   :config
+  (lsp-treemacs-sync-mode 1)
   ;;   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
   :hook ((lsp-mode . lsp-ui-mode)
 	 ;; (lsp-mode . lsp-enable-which-key-integration)
@@ -189,6 +322,9 @@
 	 :gdbpath "rust-lldb"
          :target nil
          :cwd nil)))
+
+;; (use-package init-cc-mode
+;;   :load-path "./lisp")
 
 (provide 'init-prog-mode)
 ;;; init-prog-mode.el ends here
